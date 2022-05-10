cd6fa87f8885e3c28e93348bf1508217daf57fef
Msdalmajdi <msdalmajdi@hotmail.com>
2022-05-10 20:25:43 +0300

    FINISHED Part 2

--- src/Components/IslandForm.js
+++ src/Components/IslandForm.js
@@ -4,9 +4,28 @@ export default function IslandForm({ island }) {
       <h2>{island.name}</h2>
       <img src={island.img} alt={island.name} />
       <h3>Book a trip to {island.name} island</h3>
-      <input placeholder="Type Full Name" />
-      <input placeholder="Type Phone Number" />
-      <button className="book" onClick={() => {}}>
+      <input id="name" placeholder="Type Full Name" required />
+      <input
+        id="phone"
+        type="tel"
+        placeholder="xxxx-xxxx"
+        pattern="[0-9]{4}-[0-9]{4}"
+        required
+      />
+      <button
+        className="book"
+        onClick={() => {
+          if (
+            window.confirm(
+              `Are you sure you want to book to ${island.name} with the Name: ${
+                document.getElementById("name").value
+              }, phone: ${document.getElementById("phone").value}`
+            )
+          ) {
+            alert("good job");
+          }
+        }}
+      >
         Book for today!
       </button>
     </div>
